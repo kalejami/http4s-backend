2c3ae149a9c2320d8b1c025c8f28e1204744288c
kalejami <kalejami@kalejami.com>
2018-05-13 11:22:26 +0200

    Logback classic, Flyway, Doobie, H2

--- build.sbt
+++ build.sbt
@@ -11,7 +11,11 @@ libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-blaze-server" % http4sVersion,
   "org.http4s" %% "http4s-circe" % http4sVersion,
   "io.circe" %% "circe-generic" % "0.9.3",
-  "io.circe" %% "circe-literal" % "0.9.3"
+  "io.circe" %% "circe-literal" % "0.9.3",
+  "org.tpolecat" %% "doobie-core" % "0.5.2",
+  "com.h2database" % "h2" % "1.4.197",
+  "org.flywaydb" % "flyway-core" % "5.0.7",
+  "ch.qos.logback" % "logback-classic" % "1.2.3"
 )
 
 scalacOptions ++= Seq("-Ypartial-unification")
--- src/main/scala/de/mk/sb/Main.scala
+++ src/main/scala/de/mk/sb/Main.scala
@@ -3,6 +3,10 @@ package de.mk.sb
 import java.util.UUID
 
 import cats.effect.IO
+import doobie._
+import doobie.free.connection
+import doobie.implicits._
+import doobie.util.transactor.Transactor.Aux
 import fs2.{Stream, StreamApp}
 import fs2.StreamApp.ExitCode
 import org.http4s.server.blaze._
@@ -11,6 +15,7 @@ import io.circe.syntax._
 import org.http4s._
 import org.http4s.dsl.io._
 import io.circe.generic.auto._
+import org.flywaydb.core.Flyway
 import org.http4s.circe._
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -19,10 +24,32 @@ final case class Comment(userId: String, id: String, title: String, body: String
 
 object Main extends StreamApp[IO] {
 
+  val xa: Aux[IO, Unit] = Transactor.fromDriverManager[IO](
+    "org.h2.Driver", "jdbc:h2:mem:test", "sa", "sa"
+  )
+
+  val migrate: ConnectionIO[Int] = connection.delay {
+    val flyway = new Flyway()
+    flyway.setDataSource("jdbc:h2:mem:test", "sa", "sa")
+    flyway.migrate()
+  }
+
+  val uuid: ConnectionIO[UUID] = connection.delay(UUID.randomUUID())
+
+  def byTitle(title: String): Query0[Comment] = sql"SELECT * FROM COMMENT WHERE TITLE=$title".query
+  def insert(comment: Comment): Update0 = sql"INSERT INTO COMMENT VALUES(${comment.userId}, ${comment.id}, ${comment.title}, ${comment.body})".update
+
   val helloWorldService: HttpService[IO] = HttpService[IO] {
     case GET -> Root / "comments" / name =>
-      val id = UUID.randomUUID().toString
-      Ok(List(Comment(id, id, name, name)).asJson)
+      val p = for {
+        _ <- migrate
+        id1 <- uuid
+        _ <- insert(Comment(id1.toString, id1.toString, name, id1.toString)).run
+        id2 <- uuid
+        _ <- insert(Comment(id2.toString, id2.toString, name, id2.toString)).run
+        comments <- byTitle(name).to[List]
+      } yield comments
+      Ok(p.transact(xa).map(_.asJson))
   }
 
   override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] =
